c85ea2721eb549a9344498e5ee36c394152370e2
snower <sujian199@gmail.com>
2023-03-06 17:58:02 +0800

    Add import python module example

---
+++ examples/import_python/import_python.sql
@@ -0,0 +1,6 @@
+
+use `utils`;
+
+select utils$hello();
+
+select utils$add_number(1, 2), utils$sum_array((1, 2, 3));
---
+++ examples/import_python/utils.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# 2023/3/6
+# create by: snower
+
+def hello():
+    return "hello world!"
+
+def add_number(x, y):
+    return x + y
+
+def sum_array(x):
+    return sum(x)
