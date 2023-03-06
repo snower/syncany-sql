f4b103c16a0b7270b8e86dd702b6a3d4071cc6fb
snower <sujian199@gmail.com>
2023-03-06 17:57:11 +0800

    Optimize MysqlCalculater execution and add exception logging

--- syncanysql/calculaters/mysql_calculater.py
+++ syncanysql/calculaters/mysql_calculater.py
@@ -2,6 +2,8 @@
 # 2023/3/2
 # create by: snower
 
+import traceback
+from syncany.logger import get_logger
 from syncany.calculaters.calculater import Calculater
 from . import mysql_funcs
 
@@ -12,6 +14,13 @@ class MysqlCalculater(Calculater):
     def calculate(self):
         func_name = self.name[7:]
         try:
+            if len(self.args) == 1 and isinstance(self.args[0], list) and self.args[0] and isinstance(self.args[0][0], dict):
+                try:
+                    self.funcs[func_name](*tuple(self.args[0]))
+                except TypeError:
+                    pass
             return self.funcs[func_name](*tuple(self.args))
-        except:
+        except Exception as e:
+            get_logger().warning("mysql calculater execute %s(%s) error: %s\n%s", func_name, self.args, e,
+                                 traceback.format_exc())
             return None
