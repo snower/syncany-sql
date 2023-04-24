e6a98eede69a7aaea1db71eb37ea494f5e3a32ad
snower <sujian199@gmail.com>
2023-04-24 17:16:58 +0800

    Optimize test cases

--- tests/test_example_type_annotation.py
+++ tests/test_example_type_annotation.py
@@ -5,7 +5,6 @@
 import datetime
 import uuid
 from bson.objectid import ObjectId
-from syncany.utils import get_timezone
 from .example import ExampleTestCase
 
 
@@ -17,12 +16,14 @@ class TypeAnnotationExampleTestCase(ExampleTestCase):
 
         self.assert_data(5, [{'t': 'str', 'v': '3243243'}], "data error")
         self.assert_data(7, [{'t': 'str', 'v': '12.90'}], "data error")
-        self.assert_data(10, [{'t': 'datetime', 'v': datetime.datetime(2023, 2, 23, 10, 23, 21,
-                                                                       tzinfo=get_timezone())}], "data error")
-        self.assert_data(12, [{'orderAt': datetime.datetime(2023, 2, 23, 10, 23, 21, tzinfo=get_timezone()),
-                               't': 'str', 'v': '2023-02-23'}], "data error")
-        self.assert_data(16, [{'orderAt': datetime.datetime(2023, 2, 23, 10, 23, 21, tzinfo=get_timezone()),
-                               't': 'int', 'v': 1677119001}], "data error")
+        self.assert_value(10, 't', 'datetime', "data error")
+        self.assert_value(10, 'v', lambda value: isinstance(value, datetime.datetime), "data error")
+        self.assert_value(12, 'orderAt', lambda value: isinstance(value, datetime.datetime), "data error")
+        self.assert_value(12, 't', 'str', "data error")
+        self.assert_value(12, 'v', '2023-02-23', "data error")
+        self.assert_value(16, 'orderAt', lambda value: isinstance(value, datetime.datetime), "data error")
+        self.assert_value(16, 't', 'int', "data error")
+        self.assert_value(16, 'v', 1677119001, "data error")
         self.assert_data(21, [{'t': 'objectid', 'v': ObjectId('6422a08fb4055348da72633b')},
                               {'t': 'objectid', 'v': ObjectId('6422a0a4b4055348da72633c')}], "data error")
         self.assert_data(26, [{'t': 'uuid', 'v': uuid.UUID('da984ae6-cd3f-11ed-af1b-eb91b1b4fa12')}], "data error")
