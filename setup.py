301fd687893e92c706d693c9bc172750b8c7e4ec
snower <sujian199@gmail.com>
2023-05-26 15:24:36 +0800

    Adjustment locking sqlglot version

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     zip_safe=False,
     install_requires=[
         "pyyaml>=5.1.2",
-        "sqlglot>=10.6.2,<12",
+        "sqlglot>=11.5.5,<12",
         "syncany>=0.2.9",
         'Pygments>=2.14.0',
         'prompt-toolkit>=3.0.36',
