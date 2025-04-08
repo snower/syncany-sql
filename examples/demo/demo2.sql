17f3f1c3a37e8b9dd739073eb89baf82fb754ca1
snower <sujian199@gmail.com>
2025-04-08 09:44:17 +0800

    Optimize the execute task to support the syncany json task

--- examples/demo/demo2.sql
+++ examples/demo/demo2.sql
@@ -7,3 +7,7 @@ select * from `data/orders.json` order by amount desc limit 2;
 select a.order_id, a.site_id, a.amount * 100 as amount from `data/orders.json` a order by site_id desc, a.order_id desc limit 2;
 
 select a.order_id, a.site_id, a.amount * 100 as amount from `data/orders.json` a order by a.amount * 100 desc limit 2;
+
+execute `demo.sql`;
+
+execute `json/demo.json`;
--- syncanysql/taskers/execute.py
+++ syncanysql/taskers/execute.py
@@ -4,6 +4,7 @@
 
 import os
 import time
+import json
 from syncany.logger import get_logger
 from ..parser import FileParser
 
@@ -14,32 +15,55 @@ class ExecuteTasker(object):
         self.executor = None
 
     def start(self, name, executor, session_config, manager, arguments):
-        if self.config["filename"].endswith("sql") or self.config["filename"].endswith("sqlx"):
-            if self.executor is None:
-                from ..executor import Executor
-                self.executor = Executor(manager, session_config.session(), executor)
+        start_time = time.time()
+        try:
+            if self.config["filename"].endswith(".sql") or self.config["filename"].endswith(".sqlx") or self.config["filename"].endswith(".prql"):
+                if self.executor is None:
+                    from ..executor import Executor
+                    self.executor = Executor(manager, session_config.session(), executor)
 
-            start_time = time.time()
-            get_logger().info("execute file %s", self.config["filename"])
-            try:
-                file_parser = FileParser(self.config["filename"])
-                sqls = file_parser.load()
+                get_logger().info("execute file %s", self.config["filename"])
+                try:
+                    file_parser = FileParser(self.config["filename"])
+                    sqls = file_parser.load()
+                    with self.executor as executor:
+                        executor.run(self.config["filename"], sqls)
+                finally:
+                    session_config.merged_config = None
+            elif self.config["filename"].endswith(".json"):
+                if self.executor is None:
+                    from ..executor import Executor
+                    self.executor = Executor(manager, session_config.session(), executor)
+
+                from .query import QueryTasker
+                with open(self.config["filename"], 'r', encoding=os.environ.get("SYNCANYENCODING", "utf-8")) as fp:
+                    tasker = QueryTasker(json.loads(fp.read()))
                 with self.executor as executor:
-                    executor.run(self.config["filename"], sqls)
-            finally:
-                session_config.merge()
-                get_logger().info("execute file %s finish %.2fms", self.config["filename"], (time.time() - start_time) * 1000)
+                    arguments = {"@verbose": arguments.get("@verbose", False), "@timeout": arguments.get("@timeout", 0),
+                                 "@limit": self.executor.env_variables.get("@limit", 0), "@batch": self.executor.env_variables.get("@batch", 0),
+                                 "@streaming": arguments.get("@streaming", False), "@recovery": arguments.get("@recovery", False),
+                                 "@join_batch": arguments.get("@join_batch", 10000), "@insert_batch": arguments.get("@insert_batch", 0),
+                                 "@use_input": self.executor.env_variables.get("@use_input", None), "@use_output": self.executor.env_variables.get("@use_output", None),
+                                 "@use_output_type": self.executor.env_variables.get("@use_output_type", None), "@primary_order": False}
+                    name = "execute_" + "".join([c if c.isalpha() or c.isdigit() else '_' for c in self.config["filename"]])
+                    executor.runners.extend(tasker.start(name, executor, executor.session_config, executor.manager, arguments))
+        finally:
+            get_logger().info("execute %s start %.2fms", self.config["filename"], (time.time() - start_time) * 1000)
         return [self]
 
     def run(self, executor, session_config, manager):
-        if not self.executor:
-            try:
-                os.system(self.config["filename"])
-            except Exception as e:
-                get_logger().warning("execute system command error: %s", str(e))
-            return
-        with self.executor as executor:
-            return executor.execute()
+        start_time = time.time()
+        try:
+            if not self.executor:
+                try:
+                    os.system(self.config["filename"])
+                except Exception as e:
+                    get_logger().warning("execute system command error: %s", str(e))
+                return
+            with self.executor as executor:
+                return executor.execute()
+        finally:
+            get_logger().info("execute %s run %.2fms", self.config["filename"], (time.time() - start_time) * 1000)
 
     def terminate(self):
         if not self.executor:
